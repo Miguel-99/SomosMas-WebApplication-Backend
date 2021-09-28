29c3db442c98758f58ca485416b0c8d8efe93eb2
Turco Amado <yselene@pdssa.com.ar>
2021-09-28 14:36:09 -0300

    project initialized

---
+++ src/main/java/com/alkemy/java/JavaApplication.java
@@ -0,0 +1,22 @@
+package com.alkemy.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+	}
+}
