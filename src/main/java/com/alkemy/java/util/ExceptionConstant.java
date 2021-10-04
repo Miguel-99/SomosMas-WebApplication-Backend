c0cd99ac61ae36a926af36538144d4192dd24343
matias <cabralmatiasezequiel@gmail.com>
2021-10-04 15:42:26 -0300

    chore: NOT_FOUND added

--- src/main/java/com/alkemy/java/util/ExceptionConstant.java
+++ src/main/java/com/alkemy/java/util/ExceptionConstant.java
@@ -3,5 +3,6 @@ package com.alkemy.java.util;
 public class ExceptionConstant {
 
     public final static String USERNAME_NOT_FOUND = "UsernameNotFoundException";
+    public final static String NOT_FOUND = "NotFoundException";
     public final static String INVALID_ROL = "errorInvalidRol";
 }
