18cabb631cac28f1194d4375e640287ad54b4c66
matias <cabralmatiasezequiel@gmail.com>
2021-10-05 16:57:47 -0300

    feat: role service impl added

---
+++ src/main/java/com/alkemy/java/service/impl/RoleServiceImpl.java
@@ -0,0 +1,35 @@
+package com.alkemy.java.service.impl;
+
+import com.alkemy.java.dto.RoleDto;
+import com.alkemy.java.model.Role;
+import com.alkemy.java.repository.RoleRepository;
+import com.alkemy.java.service.IRoleService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements IRoleService {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    private ModelMapper mapper;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Role registerRole (RoleDto roleRequest){
+
+        Role role = mapToEntity(roleRequest);
+        role.setName(roleRequest.getName());
+        return roleRepository.save(role);
+    }
+
+    private Role mapToEntity(RoleDto roleRequest) {
+        return mapper.map(roleRequest, Role.class);
+    }
+}
