41c524e54daf17bf35c087809ffe5dc396b8052c
matias <cabralmatiasezequiel@gmail.com>
2021-10-06 16:07:49 -0300

    chore: method added to interface

--- src/main/java/com/alkemy/java/service/IUserService.java
+++ src/main/java/com/alkemy/java/service/IUserService.java
@@ -6,5 +6,5 @@ import com.alkemy.java.dto.UserDtoResponse;
 public interface IUserService {
 
      UserDtoResponse registerUser(UserDtoRequest user);
-
+     boolean validedRole(Long id, String token);
 }
