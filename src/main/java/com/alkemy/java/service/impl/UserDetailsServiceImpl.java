82e70c02578ddd76bc21af6658bd807aa3e77f6e
matias <cabralmatiasezequiel@gmail.com>
2021-10-06 15:11:59 -0300

    fix: message path error fixed

--- src/main/java/com/alkemy/java/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/alkemy/java/service/impl/UserDetailsServiceImpl.java
@@ -1,10 +1,9 @@
 package com.alkemy.java.service.impl;
 
-import com.alkemy.java.model.UserDetail;
 import com.alkemy.java.repository.UserRepository;
-import com.alkemy.java.util.Roles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -14,12 +13,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Service
+@PropertySource("classpath:messages/error.properties")
 public class UserDetailsServiceImpl implements UserDetailsService{
 
     @Autowired
     private UserRepository userRepository;
 
-    @Value("error.user.unregister")
+    @Value("${error.user.unregister}")
     private String errorUserUnregister;
 
     @Override
@@ -31,11 +31,10 @@ public class UserDetailsServiceImpl implements UserDetailsService{
             userBuilder = User.withUsername(email);
             userBuilder.disabled(false);
             userBuilder.password(user.getPassword());
-            userBuilder.authorities(new SimpleGrantedAuthority(/*"ROLE_" + Roles.CLIENT*/ user.getRole().getName().toUpperCase()));
+            userBuilder.authorities(new SimpleGrantedAuthority(user.getRole().getName().toUpperCase()));
         } else {
             throw new UsernameNotFoundException(errorUserUnregister);
         }
-        //return userBuilder.build();
-        return new UserDetail(userRepository.findByEmail(email));
+        return userBuilder.build();
     }
 }
