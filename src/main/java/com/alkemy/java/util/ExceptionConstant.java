811bd549eb2369e84859975bbc0b9d4cb091bd13
matias <cabralmatiasezequiel@gmail.com>
2021-10-07 13:21:57 -0300

    chore: new constant added

--- src/main/java/com/alkemy/java/util/ExceptionConstant.java
+++ src/main/java/com/alkemy/java/util/ExceptionConstant.java
@@ -5,4 +5,6 @@ public class ExceptionConstant {
     public final static String USERNAME_NOT_FOUND = "UsernameNotFoundException";
     public final static String NOT_FOUND = "NotFoundException";
     public final static String INVALID_ROL = "errorInvalidRol";
+    public final static String FORBIDDEN = "ForbiddenException";
+    public final static String RESOURCE_NOT_FOUND = "ResourceNotFoundException";
 }
