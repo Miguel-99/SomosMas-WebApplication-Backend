de773311b487390b3479f3155f151d33638f3dae
Emanuel Sceppaquercia <emanuel.sceppaquercia@gmail.com>
2021-10-09 04:30:43 -0300

    Code refactoring in News Controller

--- src/main/java/com/alkemy/java/controller/NewsController.java
+++ src/main/java/com/alkemy/java/controller/NewsController.java
@@ -1,13 +1,17 @@
 package com.alkemy.java.controller;
 
 import com.alkemy.java.dto.NewsRequestDto;
+import com.alkemy.java.exception.BadRequestException;
+import com.alkemy.java.exception.InvalidDataException;
 import com.alkemy.java.service.INewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/news")
@@ -17,7 +21,9 @@ public class NewsController {
     INewsService newsService;
 
     @PostMapping
-    public ResponseEntity<?> createNews(@Valid @RequestBody NewsRequestDto newsRequestDto) {
+    public ResponseEntity<?> createNews(@Valid @RequestBody NewsRequestDto newsRequestDto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            throw new InvalidDataException(bindingResult);
 
         return new ResponseEntity<>(newsService.save(newsRequestDto),HttpStatus.CREATED);
     }
