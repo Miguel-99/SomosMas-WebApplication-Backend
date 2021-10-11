2ec90a3afdba649993980ef8689aaea2b6a34d12
Sergio Daniel Lewczuk <sergio.lewczuk@gmail.com>
2021-10-11 13:37:59 -0300

    Conflicts Resolved

--- src/main/java/com/alkemy/java/config/SendGridConfig.java
+++ src/main/java/com/alkemy/java/config/SendGridConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:sendgrid.properties")
 public class SendGridConfig {
 
     @Value("${app.sendgrid.key}")
--- src/main/java/com/alkemy/java/dto/UserDtoRequest.java
+++ src/main/java/com/alkemy/java/dto/UserDtoRequest.java
@@ -3,10 +3,7 @@ package com.alkemy.java.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
@@ -34,7 +31,7 @@ public class UserDtoRequest {
             message = "{error.size.password}")
     private String password;
 
-    @NotEmpty(message = "Field idRole should not be null or empty")
+    @NotNull(message = "Field idRole should not be null or empty")
     private Long idRole;
 
 }
