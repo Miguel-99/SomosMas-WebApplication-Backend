4f32e5f85a79dcd5efd5ff815c651142fc72c526
Sergio Daniel Lewczuk <sergio.lewczuk@gmail.com>
2021-10-11 14:58:56 -0300

    feat: function findById added.

--- src/main/java/com/alkemy/java/service/INewsService.java
+++ src/main/java/com/alkemy/java/service/INewsService.java
@@ -1,8 +1,13 @@
-
 package com.alkemy.java.service;
 
+
+import com.alkemy.java.dto.NewsDtoResponse;
+import com.alkemy.java.exception.ResourceNotFoundException;
 
 public interface INewsService {
         
     void deleteNews(Long id);
+
+    NewsDtoResponse findById(Long id) throws ResourceNotFoundException;
+
 }
