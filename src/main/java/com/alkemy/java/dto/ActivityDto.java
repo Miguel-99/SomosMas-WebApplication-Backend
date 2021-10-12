a2bd01653d0fdece6e160ea9399648592159aa58
Emanuel Sceppaquercia <emanuel.sceppaquercia@gmail.com>
2021-10-12 19:02:39 -0300

    Activity dto added

--- src/main/java/com/alkemy/java/dto/ActivityDto.java
+++ src/main/java/com/alkemy/java/dto/ActivityDto.java
@@ -1,25 +1,41 @@
 package com.alkemy.java.dto;
 
+import com.alkemy.java.model.Activity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ActivityRequestDto {
+public class ActivityDto {
 
-    @NotEmpty(message = "error.name.empty")
-    @Size(min = 5, max = 50, message = "error.name.size")
+    @NotNull(message = "{error.name.empty}")
+    @Size(min = 3, max = 50, message = "{error.name.size}")
     private String name;
 
-    @NotEmpty(message = "error.content.empty")
+    @NotEmpty(message = "{error.content.empty}")
     private String content;
 
-    @NotEmpty(message = "error.image.empty")
+    @NotEmpty(message = "{error.image.empty}")
     private String image;
+
+    public static Activity dtoToActivity(ActivityDto dto){
+        return Activity.builder()
+                .name(dto.getName())
+                .content(dto.getContent())
+                .image(dto.getImage())
+                .createDate(new Date())
+                .updateDate(new Date())
+                .deleted(false)
+                .build();
+    }
 
 }
