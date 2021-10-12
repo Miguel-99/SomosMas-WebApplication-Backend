4eb721053eb388323a45e490f1985f107aae9640
matias <cabralmatiasezequiel@gmail.com>
2021-10-12 18:08:09 -0300

    chore: PreAuthorize added

--- src/main/java/com/alkemy/java/controller/SlideController.java
+++ src/main/java/com/alkemy/java/controller/SlideController.java
@@ -4,6 +4,7 @@ import com.alkemy.java.service.ISlideService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,8 +15,8 @@ public class SlideController {
     ISlideService slideService;
 
     @GetMapping()
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> getAllSlides(){
         return new ResponseEntity<>(slideService.getAllSlide(), HttpStatus.OK);
     }
-
 }
