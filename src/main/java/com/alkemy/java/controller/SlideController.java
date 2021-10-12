d1d4e6f8cfab39347e061dbf883a3d2f7b5b79a9
matias <cabralmatiasezequiel@gmail.com>
2021-10-12 18:45:16 -0300

    chore: deleteById method added

--- src/main/java/com/alkemy/java/controller/SlideController.java
+++ src/main/java/com/alkemy/java/controller/SlideController.java
@@ -2,10 +2,14 @@ package com.alkemy.java.controller;
 
 import com.alkemy.java.service.ISlideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/slides")
@@ -14,9 +18,21 @@ public class SlideController {
     @Autowired
     ISlideService slideService;
 
+    @Autowired
+    MessageSource messageSource;
+
+    @Value("success.deleted")
+    private String deletedMessage;
+
     @GetMapping()
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> getAllSlides(){
         return new ResponseEntity<>(slideService.getAllSlide(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+        slideService.delete(id);
+        return new ResponseEntity<>(messageSource.getMessage(deletedMessage, null, Locale.getDefault()), HttpStatus.OK);
     }
 }
