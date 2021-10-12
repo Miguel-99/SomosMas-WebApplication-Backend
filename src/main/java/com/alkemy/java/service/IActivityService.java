88c3c6573eea60b6e390d3d535635dee7c2d54dc
Emanuel Sceppaquercia <emanuel.sceppaquercia@gmail.com>
2021-10-12 19:01:48 -0300

    Activity service interface and implementation, and creteActivity method added

--- src/main/java/com/alkemy/java/service/IActivityService.java
+++ src/main/java/com/alkemy/java/service/IActivityService.java
@@ -1,2 +1,10 @@
-package com.alkemy.java.service;public interface IActivityService {
+package com.alkemy.java.service;
+
+import com.alkemy.java.dto.ActivityDto;
+
+public interface IActivityService {
+
+    void createActivity(ActivityDto activityDto);
+
+
 }
--- src/main/java/com/alkemy/java/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/alkemy/java/service/impl/ActivityServiceImpl.java
@@ -1,2 +1,31 @@
-package com.alkemy.java.service.impl;public class ActivityServiceImpl {
+package com.alkemy.java.service.impl;
+
+import com.alkemy.java.dto.ActivityDto;
+import com.alkemy.java.exception.ConflictException;
+import com.alkemy.java.repository.ActivityRepository;
+import com.alkemy.java.service.IActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+public class ActivityServiceImpl implements IActivityService {
+
+    @Autowired
+    ActivityRepository activityRepository;
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    public void createActivity(ActivityDto activityDto) {
+
+        if(activityRepository.existsByName(activityDto.getName()))
+            throw new ConflictException(messageSource.getMessage("error.activity.already.exist", null, Locale.getDefault()));
+
+        activityRepository.save(ActivityDto.dtoToActivity(activityDto));
+    }
+
 }
