f4438094eb59ebdfd70e7f3cfa406f293be2e4d9
Sergio Daniel Lewczuk <sergio.lewczuk@gmail.com>
2021-10-13 08:53:48 -0300

    Fix function name findById to findNewsById

--- src/main/java/com/alkemy/java/service/INewsService.java
+++ src/main/java/com/alkemy/java/service/INewsService.java
@@ -1,13 +1,18 @@
 package com.alkemy.java.service;
 
+import com.alkemy.java.dto.NewsRequestDto;
+import com.alkemy.java.dto.NewsResponseDto;
 
-import com.alkemy.java.dto.NewsDtoResponse;
+import com.alkemy.java.dto.NewsDto;
 import com.alkemy.java.exception.ResourceNotFoundException;
 
 public interface INewsService {
-        
+
+    NewsResponseDto createNews(NewsRequestDto news);
+
     void deleteNews(Long id);
 
-    NewsDtoResponse findById(Long id) throws ResourceNotFoundException;
+    NewsDto updateNews(Long id, NewsDto newsDto);
 
+    NewsResponseDto findNewsById(Long id) throws ResourceNotFoundException;
 }
