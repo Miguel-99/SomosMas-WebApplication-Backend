d1ade7915a9b8b74f148d00be39dea1389c88c79
Sergio Daniel Lewczuk <sergio.lewczuk@gmail.com>
2021-10-13 17:29:16 -0300

    Feature: Add updateSlide(Long, Dto) function.

--- src/main/java/com/alkemy/java/service/ISlideService.java
+++ src/main/java/com/alkemy/java/service/ISlideService.java
@@ -1,5 +1,6 @@
 package com.alkemy.java.service;
 
+import com.alkemy.java.dto.SlideDto;
 import com.alkemy.java.dto.SlideResponseDto;
 
 import java.util.List;
@@ -8,4 +9,5 @@ public interface ISlideService {
 
     List<SlideResponseDto> getAllSlide();
     void delete(Long id);
+    SlideDto updateSlide (Long id, SlideDto slideDto);
 }
