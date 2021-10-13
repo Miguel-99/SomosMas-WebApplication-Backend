b0a960641fbbd507f17c9f23898d8eec844b1994
matias <cabralmatiasezequiel@gmail.com>
2021-10-13 14:58:18 -0300

    chore: success.get message added if is empty

--- src/main/java/com/alkemy/java/controller/MemberController.java
+++ src/main/java/com/alkemy/java/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.alkemy.java.controller;
 
+import com.alkemy.java.dto.MemberResponseDto;
 import com.alkemy.java.service.IMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -9,16 +12,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Locale;
+
 @RestController
 @RequestMapping("/members")
 public class MemberController {
 
     @Autowired
-    IMemberService memberService;
+    private IMemberService memberService;
+
+    @Value("success.get")
+    private String successGet;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping()
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> getAllMembers(){
-        return new ResponseEntity<>(memberService.getAllMembers(), HttpStatus.OK);
+
+        List<MemberResponseDto> members = memberService.getAllMembers();
+
+        if(members.isEmpty()){
+            String message = messageSource.getMessage(successGet, null, Locale.getDefault());
+            return new ResponseEntity<>(message, HttpStatus.ACCEPTED);
+        }
+
+        return new ResponseEntity<>(members, HttpStatus.OK);
     }
 }
