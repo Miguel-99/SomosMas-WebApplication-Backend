c6f6acd7643d0828f60d4127885377f154875bef
Sergio Daniel Lewczuk <sergio.lewczuk@gmail.com>
2021-10-13 17:56:04 -0300

    Add function updateTestimonial(id, dto)

--- src/main/java/com/alkemy/java/service/ITestimonialService.java
+++ src/main/java/com/alkemy/java/service/ITestimonialService.java
@@ -1,8 +1,12 @@
 package com.alkemy.java.service;
 
 
+import com.alkemy.java.dto.TestimonialDto;
+
 public interface ITestimonialService {
 
     void deleteById(Long id);
+
+    TestimonialDto updateTestimonial(Long id, TestimonialDto testimonialDto);
 
 }
