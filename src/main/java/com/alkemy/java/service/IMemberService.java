6aeb2a169ec6ad6fd1c06c02d546474a473a1a83
giulianodanielmonti <giulianm@gmail.com>
2021-10-13 21:09:16 -0300

    added createMember to iservice

--- src/main/java/com/alkemy/java/service/IMemberService.java
+++ src/main/java/com/alkemy/java/service/IMemberService.java
@@ -1,5 +1,6 @@
 package com.alkemy.java.service;
 
+import com.alkemy.java.dto.MemberRequestDto;
 import com.alkemy.java.dto.MemberResponseDto;
 
 import java.util.List;
@@ -7,4 +8,6 @@ import java.util.List;
 public interface IMemberService {
 
     List<MemberResponseDto> getAllMembers();
+
+    MemberResponseDto createMember(MemberRequestDto org) throws Exception;
 }
