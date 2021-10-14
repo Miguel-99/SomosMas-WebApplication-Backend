5bfe9f9663532bd1ae0cbb4950fa9dd1d45b399e
Emanuel Sceppaquercia <emanuel.sceppaquercia@gmail.com>
2021-10-14 18:57:04 -0300

    New method added to slide repository

--- src/main/java/com/alkemy/java/repository/SlideRepository.java
+++ src/main/java/com/alkemy/java/repository/SlideRepository.java
@@ -2,9 +2,18 @@ package com.alkemy.java.repository;
 
 import com.alkemy.java.model.Slide;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SlideRepository extends JpaRepository<Slide, Long> {
+
+    @Query("SELECT m FROM Slide m JOIN m.organizationId n WHERE n.id LIKE :id ORDER BY m.numberOrder")
+    List<Slide> findAllByOrganizationId(@Param("id") Long id);
+
+
 
 }
