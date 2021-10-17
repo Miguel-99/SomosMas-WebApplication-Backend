31bdf36dcc6316c82c135141d6eddf86fe5b8723
matias <cabralmatiasezequiel@gmail.com>
2021-10-17 20:33:27 -0300

    chore: new constructor added to activity

--- src/main/java/com/alkemy/java/model/Activity.java
+++ src/main/java/com/alkemy/java/model/Activity.java
@@ -1,7 +1,9 @@
 package com.alkemy.java.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import javax.persistence.*;
@@ -11,10 +13,12 @@ import java.util.Date;
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table (name = "activities")
 @SQLDelete(sql = "UPDATE activities SET deleted = true WHERE id=?")
 @Where(clause = " deleted = false ")
-public class Activity  {
+public class Activity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,14 +34,19 @@ public class Activity  {
     @NotBlank(message = "Field image should not be null or empty")
     private String image;
 
-    @Temporal(TemporalType.DATE)
     @Column (name = "create_date")
     private Date createDate;
 
-    @Temporal(TemporalType.DATE)
     @Column (name = "update_date")
     private Date updateDate;
 
     private Boolean deleted = Boolean.FALSE;
 
+    public Activity(String name, String content, String image, Date createDate, Date updateDate) {
+        this.name = name;
+        this.content = content;
+        this.image = image;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
 }
