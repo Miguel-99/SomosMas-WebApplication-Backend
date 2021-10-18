bd94707bd4d922200342d0f51b1ad346fb0ee337
Sergio Daniel Lewczuk <sergio.lewczuk@gmail.com>
2021-10-18 12:57:09 -0300

    DataSeed for Users Add implementations :     - Role findByName(String name)     - boolean existsByName(String name) : Check if Role exist by name     - Role registerRole(Role role) : Register Role

--- src/main/java/com/alkemy/java/service/impl/RoleServiceImpl.java
+++ src/main/java/com/alkemy/java/service/impl/RoleServiceImpl.java
@@ -1,12 +1,17 @@
 package com.alkemy.java.service.impl;
 
 import com.alkemy.java.dto.RoleDto;
+import com.alkemy.java.exception.ResourceNotFoundException;
 import com.alkemy.java.model.Role;
 import com.alkemy.java.repository.RoleRepository;
 import com.alkemy.java.service.IRoleService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class RoleServiceImpl implements IRoleService {
@@ -16,6 +21,12 @@ public class RoleServiceImpl implements IRoleService {
 
     @Autowired
     private ModelMapper mapper;
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Value("error.role.nameNotFound")
+    String messageDontExist;
 
     public RoleServiceImpl(RoleRepository roleRepository, ModelMapper mapper) {
         this.roleRepository = roleRepository;
@@ -27,6 +38,21 @@ public class RoleServiceImpl implements IRoleService {
 
         Role role = mapToEntity(roleRequest);
         role.setName(roleRequest.getName());
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role findByName(String name) {
+        return roleRepository.findByName(name).orElseThrow(()-> new ResourceNotFoundException(messageSource.getMessage(messageDontExist, null, Locale.getDefault())));
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return roleRepository.existsByName(name);
+    }
+
+    @Override
+    public Role registerRole(Role role) {
         return roleRepository.save(role);
     }
 
