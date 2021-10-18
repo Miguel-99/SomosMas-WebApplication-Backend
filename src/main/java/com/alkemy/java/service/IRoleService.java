99f492996b17155b1194f86cfc19c672b6817a6e
Sergio Daniel Lewczuk <sergio.lewczuk@gmail.com>
2021-10-18 12:56:19 -0300

    DataSeed for Users Add functions:     - Role findByName(String name);     - boolean existsByName(String name);     - Role registerRole(Role role);

--- src/main/java/com/alkemy/java/service/IRoleService.java
+++ src/main/java/com/alkemy/java/service/IRoleService.java
@@ -6,4 +6,8 @@ import com.alkemy.java.model.Role;
 public interface IRoleService {
 
     Role registerRole (RoleDto roleRequest);
+
+    Role findByName(String name);
+    boolean existsByName(String name);
+    Role registerRole(Role role);
 }
