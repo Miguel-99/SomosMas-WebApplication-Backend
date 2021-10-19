0adaac6475ee91e9c1695c6f5a348590e5d6324a
giulianodanielmonti <giulianm@gmail.com>
2021-10-19 09:04:25 -0300

    added pagination method and custom error

--- src/main/java/com/alkemy/java/controller/CategoryController.java
+++ src/main/java/com/alkemy/java/controller/CategoryController.java
@@ -6,12 +6,20 @@ import com.alkemy.java.dto.CategoryRequestDto;
 import com.alkemy.java.dto.CategoryResponseDto;
 import com.alkemy.java.exception.InvalidDataException;
 import com.alkemy.java.exception.ResourceNotFoundException;
+import com.alkemy.java.dto.CategoryProjectionDto;
 import com.alkemy.java.service.ICategoryService;
+
 import javax.validation.Valid;
+
 import javassist.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,15 +27,16 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.Locale;
 
 
 @RestController
 @RequestMapping("/categories")
+@Slf4j
 public class CategoryController {
 
     @Autowired
@@ -40,30 +49,57 @@ public class CategoryController {
     String messageDeleted;
 
 
+    @Value("error.pagination")
+    String paginationError;
+
+
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    public ResponseEntity <?> createCategory (@Valid @RequestBody (required = true) CategoryRequestDto categoryRequest, BindingResult bindingResult){
+    public ResponseEntity<?> createCategory(@Valid @RequestBody(required = true) CategoryRequestDto categoryRequest, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
             throw new InvalidDataException(bindingResult);
 
-            return new ResponseEntity<>(iCategoryService.createCategory(categoryRequest), HttpStatus.CREATED);
+        return new ResponseEntity<>(iCategoryService.createCategory(categoryRequest), HttpStatus.CREATED);
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<CategoryListRespDto>> getAllCategories(){
-        List<CategoryListRespDto> categories  = iCategoryService.findAllCategories()
+    public ResponseEntity<List<CategoryListRespDto>> getAllCategories() {
+        List<CategoryListRespDto> categories = iCategoryService.findAllCategories()
                 .stream()
                 .map(CategoryListRespDto::new)
                 .collect(Collectors.toList());
         return categories.isEmpty() ? ResponseEntity.status(HttpStatus.NO_CONTENT).body(categories) : ResponseEntity.ok(categories);
     }
 
+
+    @GetMapping("/page")
+    ResponseEntity<?> getCategoriesPageable(@PageableDefault(sort = "id", direction = Sort.Direction.ASC, size = 10) Pageable pageable,
+                                            @RequestParam(value = "page", defaultValue = "0") int page) {
+        try {
+            Page<CategoryProjectionDto> result =
+                    iCategoryService.getPageableCategory(pageable);
+            log.info(String.valueOf(result.getTotalPages()));
+
+
+            if (page >= result.getTotalPages() | page < 0)
+                return ResponseEntity.status(HttpStatus.ACCEPTED).body(
+                        messageSource.getMessage(paginationError, null, Locale.getDefault()));
+
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
+    }
+
+
     @PutMapping("/{id}")
     public ResponseEntity<?> updateCategory(@Valid @RequestBody CategoryResponseDto categoryResponseDto, @PathVariable(name = "id") long id) {
         try {
-            return new ResponseEntity<>(iCategoryService.updateCategory(categoryResponseDto,id), HttpStatus.OK);
-        }catch(ResourceNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(iCategoryService.updateCategory(categoryResponseDto, id), HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
@@ -75,7 +111,7 @@ public class CategoryController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
-    public ResponseEntity <?> deleteCategory (@PathVariable Long id){
+    public ResponseEntity<?> deleteCategory(@PathVariable Long id) {
         iCategoryService.deleteCategory(id);
         return new ResponseEntity<>(messageSource.getMessage(messageDeleted, null, Locale.getDefault()), HttpStatus.OK);
     }
---
+++ src/main/java/com/alkemy/java/dto/CategoryProjectionDto.java
@@ -0,0 +1,6 @@
+package com.alkemy.java.repository;
+
+public interface CategoryProjectionDto{
+
+    public String getName();
+}
