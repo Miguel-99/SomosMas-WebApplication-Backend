77dba8f71ab93c0ebd9fd2719215df84d82ba32b
Emanuel Sceppaquercia <emanuel.sceppaquercia@gmail.com>
2021-10-19 00:17:48 -0300

    Comment dto added and small fixes in comment model

--- src/main/java/com/alkemy/java/dto/CommentDto.java
+++ src/main/java/com/alkemy/java/dto/CommentDto.java
@@ -1,10 +1,19 @@
 package com.alkemy.java.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
-public class CommentRequestDto {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDto {
 
-    @NotEmpty(message = "{error.body.empty}")
+    @NotEmpty(message = "{error.comment.body.empty}")
+    @Size(max = 255, message = "{error.comment.body.limit}")
     private String body;
 
 }
