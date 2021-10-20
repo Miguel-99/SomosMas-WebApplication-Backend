dc992fc39f080b03c059f152ced7a1cacb7a9181
giulianodanielmonti <giulianm@gmail.com>
2021-10-19 21:35:41 -0300

    added pageDTO

--- src/main/java/com/alkemy/java/dto/PageDto.java
+++ src/main/java/com/alkemy/java/dto/PageDto.java
@@ -1,2 +1,18 @@
-package com.alkemy.java.dto;public class PageDto {
+package com.alkemy.java.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageDto <T>{
+    private List<T> content;
+    private Map<String,String> links = new HashMap<>();
 }
+
