1d8f7b2a17b57d08424ee1eba2c5c12be6c72f5c
matias <cabralmatiasezequiel@gmail.com>
2021-10-21 15:08:07 -0300

    feat: create comment function added

--- src/main/java/com/alkemy/java/service/ICommentService.java
+++ src/main/java/com/alkemy/java/service/ICommentService.java
@@ -1,5 +1,9 @@
 package com.alkemy.java.service;
 
+import com.alkemy.java.dto.CommentRequestDto;
+import com.alkemy.java.dto.CommentResponseDto;
+
 public interface ICommentService {
 
+    CommentResponseDto createComment(CommentRequestDto commentRequestDto, String token);
 }
