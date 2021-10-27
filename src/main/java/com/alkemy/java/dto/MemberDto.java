9884ea2d512f52a29c0c26c39dc868a2fb92bcbc
giulianodanielmonti <giulianm@gmail.com>
2021-10-27 14:53:40 -0300

    added space to regex

--- src/main/java/com/alkemy/java/dto/MemberDto.java
+++ src/main/java/com/alkemy/java/dto/MemberDto.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.Pattern;
 public class MemberDto {
 
     @NotEmpty(message = "{error.member.name}")
-    @Pattern(regexp="^[A-Za-z]*$",message = "{error.member.output}")
+    @Pattern(regexp="^[A-Za-z ]*$",message = "{error.member.output}") //tiene q tener espacio
     private String name;
 
     @NotEmpty
