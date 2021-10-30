c337edf37dc662029ae1c73a9befd420d9ced7d2
Emanuel Sceppaquercia <emanuel.sceppaquercia@gmail.com>
2021-10-30 12:10:04 -0300

    NewsControllerTest Added

--- src/test/java/com/alkemy/java/config/Config.java
+++ src/test/java/com/alkemy/java/config/Config.java
@@ -1,8 +1,13 @@
 package com.alkemy.java.config;
 
+import com.alkemy.java.repository.CommentRepository;
+import com.alkemy.java.repository.NewsRepository;
 import com.alkemy.java.repository.UserRepository;
+import com.alkemy.java.service.ICommentService;
+import com.alkemy.java.service.INewsService;
 import com.alkemy.java.service.impl.UserDetailsServiceImpl;
 import com.alkemy.java.util.JwtUtil;
+import com.alkemy.java.util.UtilPagination;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -10,15 +15,27 @@ import org.springframework.context.annotation.Bean;
 @TestConfiguration
 public class Config {
     @Bean
-    public UserDetailsServiceImpl userService(){
-        return Mockito.mock(UserDetailsServiceImpl.class);
-    }
-
-    @Bean
     public JwtUtil jwt(){
         return Mockito.mock(JwtUtil.class);
     }
+    @Bean
+    public UtilPagination utilPagination() { return Mockito.mock(UtilPagination.class); }
 
     @Bean
+    public UserDetailsServiceImpl userService(){
+        return Mockito.mock(UserDetailsServiceImpl.class);
+    }
+    @Bean
     public UserRepository userRepository() { return Mockito.mock(UserRepository.class);}
+
+    @Bean
+    public NewsRepository newsRepository() { return Mockito.mock(NewsRepository.class);}
+    @Bean
+    public INewsService newsService() { return Mockito.mock(INewsService.class);}
+
+    @Bean
+    public CommentRepository commentRepository() { return Mockito.mock(CommentRepository.class);}
+    @Bean
+    public ICommentService commentService() { return Mockito.mock(ICommentService.class);}
+
 }
