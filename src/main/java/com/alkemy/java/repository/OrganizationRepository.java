500d3d26bcd2bbde89b779b83d396160dcd6c1e3
matias <cabralmatiasezequiel@gmail.com>
2021-10-30 20:44:04 -0300

    chore: set contact info by id deleted

--- src/main/java/com/alkemy/java/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/java/repository/OrganizationRepository.java
@@ -13,11 +13,6 @@ import java.util.Optional;
 @Repository
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 
-    @Modifying
-    @Query(value = "update organizations set linkedin_url = ?1, facebook_url = ?2, instagram_url = ?3 where id = ?4", nativeQuery = true)
-    @Transactional
-    void setContactInfoById(String linkedinUrl, String facebookUrl, String instagramUrl,Long id);
-
     Organization findByEmail(String email);
 
 }
